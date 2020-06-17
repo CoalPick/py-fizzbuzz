211e571a62c5676ec0742bdab5c0b8303a6f09a2
Richard W. G. Earl <58142125+CoalPick@users.noreply.github.com>
2020-06-17 20:29:40 +0100

    FizzBuzz.py - Turned the tabs into spaces

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,14 +5,14 @@ for num in range(1, 101):  # Range in Python and other languages go from a range
     
     # For each Fizz, Buzz or other conditon in this dictionary, add it to the string
     conditions = { 3 : "Fizz", 5 : "Buzz" }
-    for key in conditions:				# For numbers like 15 multiple conditions are met, so they need to be concatonated together
+    for key in conditions:              # For numbers like 15 multiple conditions are met, so they need to be concatonated together
         if (num % key) == 0:
-            strOut += conditions[key]	# Add the condition matching the key, i.e. Fizz from 3
+            strOut += conditions[key]   # Add the condition matching the key, i.e. Fizz from 3
     
     # Check if any conditions were met
     if strOut == "":
-        print (str(num))	    # Output the number if no conditions were met
+        print (str(num))        # Output the number if no conditions were met
     else:
-        print (strOut)		    # Output the string if any conditions are met
+        print (strOut)          # Output the string if any conditions are met
 
 input("Press ENTER to exit...") # Stops the program from exiting automatically
