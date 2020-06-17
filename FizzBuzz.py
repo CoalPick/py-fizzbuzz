9225e78b76c9ebaa2509834486a73d7d9d329353
Richard W. G. Earl <58142125+CoalPick@users.noreply.github.com>
2020-06-17 20:42:22 +0100

    FizzBuzz.py - Extra comment

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,13 +2,13 @@
 
 for num in range(1, 101):  # Range in Python and other languages go from a range of min to max - 1
     strOut = ""            # The string for the final output
-    
+
     # For each Fizz, Buzz or other conditon in this dictionary, add it to the string
     conditions = { 3 : "Fizz", 5 : "Buzz" }
     for key in conditions:              # For numbers like 15 multiple conditions are met, so they need to be concatonated together
-        if (num % key) == 0:
+        if (num % key) == 0:            # Check that num is divisible by the key
             strOut += conditions[key]   # Add the condition matching the key, i.e. Fizz from 3
-    
+
     # Check if any conditions were met
     if strOut == "":
         print (str(num))        # Output the number if no conditions were met
